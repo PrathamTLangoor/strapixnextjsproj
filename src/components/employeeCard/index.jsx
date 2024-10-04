4ca258a95d999784a24049cf82bef62135974810
Pratham <pratham@Apples-MacBook-Pro.local>
2024-10-04 14:55:59 +0530

    soft delete in progress

--- src/components/employeeCard/index.jsx
+++ src/components/employeeCard/index.jsx
@@ -4,10 +4,11 @@ import { HiOfficeBuilding } from "react-icons/hi";
 import { HiIdentification } from "react-icons/hi";
 import { GrUserManager } from "react-icons/gr";
 import { IoPerson } from "react-icons/io5";
-import { MdDelete } from "react-icons/md";
 import Link from 'next/link';
+import { MdDelete } from "react-icons/md";
+import { useState } from 'react';
 
-const EmployeeCard = ({ employees, managers }) => {
+const EmployeeCard = ({ employees, managers, deleteToggle }) => {
     return (
         <div className='flex flex-row gap-4 pl-3 mt-5 flex-wrap'>
             {employees &&
@@ -15,42 +16,77 @@ const EmployeeCard = ({ employees, managers }) => {
                     const manager = managers.data.find(manager => manager.attributes.manager_id === employee.attributes.manager_id);
 
                     return (
-                        <Link href={"/employee/[employeeId]"} as={`/employee/${employee.attributes.employee_id}`} key={employee.id} className='flex gap-4 justify-evenly items-center bg-black text-white focus:bg-black focus:text-white hover:bg-white border-2 border-[#000] hover:border-blue-500 focus:border-[#000] hover:text-black py-3 px-4 rounded-md w-[24%]' >
-                            <div>
-                                <Image
-                                    src={`http://localhost:1337${employee.attributes.profile_img.data.attributes.url}`}
-                                    width={75}
-                                    height={75}
-                                    className='rounded-md border border-blue-500'
-                                    alt="Employee Image"
-                                />
-                            </div>
-                            <div className='flex flex-col'>
-                                <div className='flex gap-1 items-center'>
-                                    <HiIdentification className='text-[0.8rem]' />
-                                    <span className='font-bold'>ID:</span> {employee.attributes.employee_id}
+                        <>
+                            {deleteToggle ? (
+                                <div key={employee.id} className='flex gap-4 justify-evenly items-center bg-black text-white border-2 border-[#000] py-3 px-4 rounded-md w-[24%]' >
+                                    <div>
+                                        <Image
+                                            src={`http://localhost:1337${employee.attributes.profile_img.data.attributes.url}`}
+                                            width={75}
+                                            height={75}
+                                            className='rounded-md border border-blue-500'
+                                            alt="Employee Image"
+                                        />
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <div className='flex gap-1 items-center'>
+                                            <HiIdentification className='text-[0.8rem]' />
+                                            <span className='font-bold'>ID:</span> {employee.attributes.employee_id}
+                                        </div>
+                                        <div className='flex gap-1 items-center'>
+                                            <IoPerson className='text-[0.8rem]' />
+                                            <span className='font-bold'>Name:</span> {employee.attributes.first_name} {employee.attributes.last_name}
+                                        </div>
+                                        <div className='flex gap-1 items-center'>
+                                            <MdEmail className='text-[0.8rem]' />
+                                            <span className='font-bold'>Email:</span> {employee.attributes.employee_email}
+                                        </div>
+                                        <div className='flex gap-1 items-center'>
+                                            <HiOfficeBuilding className='text-[0.8rem]' />
+                                            <span className='font-bold'>Department:</span> {employee.attributes.department}
+                                        </div>
+                                        <div className='flex gap-1 items-center'>
+                                            <GrUserManager className='text-[0.8rem]' />
+                                            <span className='font-bold'>Manager:</span> {manager.attributes.first_name} {manager.attributes.last_name}
+                                        </div>
+                                    </div>
+                                    <button className='text-red-600'><MdDelete /></button>
                                 </div>
-                                <div className='flex gap-1 items-center'>
-                                    <IoPerson className='text-[0.8rem]' />
-                                    <span className='font-bold'>Name:</span> {employee.attributes.first_name} {employee.attributes.last_name}
-                                </div>
-                                <div className='flex gap-1 items-center'>
-                                    <MdEmail className='text-[0.8rem]' />
-                                    <span className='font-bold'>Email:</span> {employee.attributes.employee_email}
-                                </div>
-                                <div className='flex gap-1 items-center'>
-                                    <HiOfficeBuilding className='text-[0.8rem]' />
-                                    <span className='font-bold'>Department:</span> {employee.attributes.department}
-                                </div>
-                                <div className='flex gap-1 items-center'>
-                                    <GrUserManager className='text-[0.8rem]' />
-                                    <span className='font-bold'>Manager:</span> {manager.attributes.first_name} {manager.attributes.last_name}
-                                </div>
-                            </div>
-                            {/* <input type='checkbox'/> */}
-                                {/* <MdDelete className='text-red-600' /> */}
-                            {/* </button> */}
-                        </Link>
+                            ) : (
+                                <Link href={"/employee/[employeeId]"} as={`/employee/${employee.attributes.employee_id}`} key={employee.employee_id} className='flex gap-4 justify-evenly items-center bg-black text-white focus:bg-black focus:text-white hover:bg-white border-2 border-[#000] hover:border-blue-500 focus:border-[#000] hover:text-black py-3 px-4 rounded-md w-[24%]' >
+                                    <div>
+                                        <Image
+                                            src={`http://localhost:1337${employee.attributes.profile_img.data.attributes.url}`}
+                                            width={75}
+                                            height={75}
+                                            className='rounded-md border border-blue-500'
+                                            alt="Employee Image"
+                                        />
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <div className='flex gap-1 items-center'>
+                                            <HiIdentification className='text-[0.8rem]' />
+                                            <span className='font-bold'>ID:</span> {employee.attributes.employee_id}
+                                        </div>
+                                        <div className='flex gap-1 items-center'>
+                                            <IoPerson className='text-[0.8rem]' />
+                                            <span className='font-bold'>Name:</span> {employee.attributes.first_name} {employee.attributes.last_name}
+                                        </div>
+                                        <div className='flex gap-1 items-center'>
+                                            <MdEmail className='text-[0.8rem]' />
+                                            <span className='font-bold'>Email:</span> {employee.attributes.employee_email}
+                                        </div>
+                                        <div className='flex gap-1 items-center'>
+                                            <HiOfficeBuilding className='text-[0.8rem]' />
+                                            <span className='font-bold'>Department:</span> {employee.attributes.department}
+                                        </div>
+                                        <div className='flex gap-1 items-center'>
+                                            <GrUserManager className='text-[0.8rem]' />
+                                            <span className='font-bold'>Manager:</span> {manager.attributes.first_name} {manager.attributes.last_name}
+                                        </div>
+                                    </div>
+                                </Link>)}
+                        </>
                     );
                 })
             }
--- src/pages/employee/index.jsx
+++ src/pages/employee/index.jsx
@@ -1,36 +1,41 @@
-import React, { useState } from 'react'
-import { fetcher } from '../../../lib/api'
+import React, { useState } from 'react';
+import { fetcher } from '../../../lib/api';
 import EmployeeCard from '@/components/employeeCard/index';
 import { IoChevronBack } from "react-icons/io5";
 import Link from 'next/link';
 
 const Employee = ({ employees, managers }) => {
-  
   const [remove, setRemove] = useState(false);
+
+  const toggleRemove = () => {
+    setRemove(prev => !prev);
+    console.log("Remove state ", !remove); 
+  };
 
   return (
     <div>
       <div className='flex justify-between'>
-        <Link href={"/"} className=' flex gap-1 items-center pt-2 pl-2 w-[85px] hover:underline hover:decoration-blue-500 underline-offset-2'>
+        <Link href={"/"} className='flex gap-1 items-center pt-2 pl-2 w-[85px] hover:underline hover:decoration-blue-500 underline-offset-2'>
           <IoChevronBack size={20} className='no-underline' />
-          <div className='text-black text-[1.2rem]'>
-            Back
-          </div>
+          <div className='text-black text-[1.2rem]'>Back</div>
         </Link>
-        <Link href={"/employee/add"} className='mr-2 mt-3 p-2 border border-black rounded-full hover:bg-black hover:text-blue-500'>
-          + Add Employee
-        </Link>
-        <button onToggle={remove === false? setRemove(true) : setRemove(false)} >
-          - Remove Employee
-        </button>
+        <div className='mr-3 mt-3 flex gap-4'>
+          <Link href={"/employee/add"} className='py-2 px-3 border border-black rounded-full hover:bg-black hover:text-blue-500'>
+            + Add Employee
+          </Link>
+          <button 
+            onClick={toggleRemove} 
+            className='py-2 px-3 border border-black rounded-full hover:bg-black hover:text-blue-500'
+          >
+            {remove ? 'Cancel Removal' : 'Remove Employee'}
+          </button>
+        </div>
       </div>
-      <div className='text-[3rem] font-semibold w-full text-center'>
-        Employees
-      </div>
-      <EmployeeCard employees={employees} managers={managers} deleteToggle={remove}/>
+      <div className='text-[3rem] font-semibold w-full text-center'>Employees</div>
+      <EmployeeCard employees={employees} managers={managers} deleteToggle={remove} />
     </div>
-  )
-}
+  );
+};
 
 export default Employee;
 
@@ -41,7 +46,7 @@ export async function getStaticProps() {
   return {
     props: {
       employees: employeeResponse,
-      managers: managerResponse
-    }
-  }
+      managers: managerResponse,
+    },
+  };
 }
